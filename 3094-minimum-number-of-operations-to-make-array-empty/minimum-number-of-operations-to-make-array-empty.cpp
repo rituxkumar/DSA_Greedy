f747d9c4d0973df95ba4437c44ea45f059054d18
Ritu Kumar <ritu748841@gmail.com>
2025-12-22 22:04:23 +0530

    Time: 15 ms (72.71%) | Memory: 95.8 MB (60.85%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        for (int& num : nums) {
+            mp[num]++;
+        }
+
+        int result = 0;
+        for (auto& it : mp) {
+            int freq = it.second;
+            if (freq == 1) {
+                return -1;
+            }
+            result += ceil((double)freq / 3);
+        }
+
+        return result;
+    }
+};
